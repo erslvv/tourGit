85d46563fa8844b32050a79de9fbf83da95dc2ae
erslvv <erslvvll@gmail.com>
2026-02-25 12:53:15 +0500

    <feat> Add config

--- src/main/java/kz/safetrip/safetrip/SafetripApplication.java
+++ src/main/java/kz/safetrip/safetrip/SafetripApplication.java
@@ -2,12 +2,14 @@ package kz.safetrip.safetrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("kz.safetrip.safetrip.repository")
+@EntityScan("kz.safetrip.safetrip.model.entity")
 public class SafetripApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SafetripApplication.class, args);
 	}
-
 }
---
+++ src/main/java/kz/safetrip/safetrip/config/OpenApiConfig.java
@@ -0,0 +1,28 @@
+package kz.safetrip.safetrip.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "SafeTrip API",
+                version = "v1",
+                description = "REST API for SafeTrip application",
+                contact = @Contact(name = "SafeTrip Team")
+        )
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
